7edd6ae1eff4b71f778a4e44b1bad9cadc847e05
Kenneth Love <kennethlove@Kenneths-MacBook-Pro.local>
2017-11-01 15:14:33 -0400

    Test commit

--- js/main.js
+++ js/main.js
@@ -4,4 +4,4 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
